1e7bb2241f02c1636bc455d0a81d4d1432d98751
BulingBulingWei <257459102@qq.com>
2022-09-05 18:15:57 +0800

    搭好了环境，里面有一些测试用的文件

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Login from "./pages/Login";
+
+function App() {
+  return <Login></Login>;
+}
+
+export default App;
---
+++ src/pages/Login/index.js
@@ -0,0 +1,7 @@
+export default function Login(props) {
+  return (
+    <div className="h-screen w-screen bg-pink-100">
+      <div>Login</div>
+    </div>
+  );
+}
